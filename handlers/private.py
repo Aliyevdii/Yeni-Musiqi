d3e26f8e51551eed368ae61a674d574a95bf9e10
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2021-10-25 01:28:24 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -2,48 +2,43 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_USARNAME, BOT_NAME as bot
-from helpers.filters import command, other_filters2
+from helpers.filters import other_filters2
 
-#start mesajı 
 
 @Client.on_message(command(["start", f"start@{BOT_USARNAME}"]))
 async def start(_, message: Message):
-  await message.reply_photo("https://i.ibb.co/MpdcXYM/IMG-20211023-135851-221.jpg")
-  await message.reply_text(
-    f"""**Merhaba, {message.from_user.mention} 🎵
-    Ben {bot}! Sesli sohbetlerde müzik çalabilen botum. Ban yetkisiz, Ses yönetimi yetkisi verip, Asistanı gruba ekleyiniz.**""",
-    reply_markup=InlineKeyboardMarkup(
-      [
-        [
-          InlineKeyboardButton(
-                        "🏷️ Destek Grubu", url="https://t.me/SohbetOdagi"
-                        ),
-                        InlineKeyboardButton(
-                        "🔧 Geliştirici", url = "https://t.me/Bir_Beyfendi"
-                        )
-                        ],
-                        [
-                          InlineKeyboardButton(
-                        "🛠 Kurucu", url = "https://t.me/Mahoaga"
-                        ),
-                        InlineKeyboardButton(
-                        "🔊 Asistan", url = "https://t.me/HerTeldenAsistan"
-                        )
-                        ],
-                        [
-                          InlineKeyboardButton(
-                        "🌀 Komutlar", url = "cbhelp"
-                        ),
-                        InlineKeyboardButton(
-                        "🎮 Oyun Botu", url="https://t.me/BasitOyunBot"
-                        )
-                        ]
-                        ]
-                        ),
-                        disable_web_page_preview=True
-                        )
+    await message.reply_photo("https://i.ibb.co/MpdcXYM/IMG-20211023-135851-221.jpg")
+    await message.reply_text(
+        f"""**Merhaba, {message.from_user.mention} 🎵
+Ben {bot}! Sesli sohbetlerde müzik çalabilen botum. Ban yetkisiz, Ses yönetimi yetkisi verip, Asistanı gruba ekleyiniz.**""",
 
-#reload mesajı
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🏷️ Destek Grubu", url="https://t.me/SohbetOdagi"
+                    ),
+                    InlineKeyboardButton(
+                        "🔧 Geliştirici", url = "https://t.me/Bir_Beyfendi"
+                    )
+                  ],[
+                    InlineKeyboardButton(
+                        "🛠 Kurucu" , url = "https://t.me/Mahoaga"
+                    ),
+                    InlineKeyboardButton(
+                        "🔊 Asistan" , url = "https://t.me/HerTeldenAsistan"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "🌀 Komutlar" , url = "https://telegra.ph/Komutlar-10-22"
+                    ),
+                    InlineKeyboardButton(
+                        "🎮 Oyun Botu", url="https://t.me/BasitOyunBot"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
 
 @Client.on_message(filters.command(["reload", f"reload@{BOT_USARNAME}"]) & ~filters.private & ~filters.channel)
 async def reload(_, message: Message):
@@ -57,3 +52,4 @@ async def reload(_, message: Message):
             ]
         )
    )
+
