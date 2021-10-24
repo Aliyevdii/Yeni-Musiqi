851ab6c75978a8eb4014b74000d6d4526d1db2d8
aylak-github <85997548+aylak-github@users.noreply.github.com>
2021-10-24 18:44:26 +0300

    Start, Help, Reload
    +Help Menüsü

--- handlers/private.py
+++ handlers/private.py
@@ -40,6 +40,36 @@ Ben {bot}! Sesli sohbetlerde müzik çalabilen botum. Ban yetkisiz, Ses yönetim
      disable_web_page_preview=True
     )
 
+@Client.on_message(filters.command(["help", f"help@{BOT_USARNAME}"]) & ~filters.private & ~filters.channel)
+async def help(_, message: Message):
+      await message.reply_text(f"""<b> Selam {message.from_user.mention}!</>\n Bu botun yardım menüsü🥳
+__
+▶️ `/oynat` - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme
+▶️ `/oynat` <şarkı ismi> - istediğiniz şarkıyı çal
+🔴 `/ytp` <Sorgu> - youtube üzerinden çalma
+🔍 `/ara` <query> - youtube'da ayrıntıları içeren videoları arama
+__
+**Yalnızca yöneticiler için..**__
+▶️ `/devam` - şarkı çalmaya devam et 
+⏩ `/atla` - sonraki şarkıyı çal 
+__
+**Asistanı grubunuza almak için..**
+__
+⚪ `/katil` - Müzik asistanı grubunuza katılır. 
+⚫ `/ayril` - Müzik asistanı grubunuzu terk eder.__""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚙ Geliştirici", url="https://t.me/Bir_Beyfendi")
+                ],
+[
+InlineKeyboardButton("🛠 Destek Grubu", url="https://t.me/SohbetOdagi")
+]
+            ]
+        )
+   )
+
 @Client.on_message(filters.command(["reload", f"reload@{BOT_USARNAME}"]) & ~filters.private & ~filters.channel)
 async def reload(_, message: Message):
       await message.reply_text("""**Yeniden başlatıldı. Bot çalışıyor ✅**""",
