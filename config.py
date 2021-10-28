3ad6e7daa8c601ed0f70d6be28edd3fd9063082f
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2021-10-28 14:16:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 BOT_USARNAME = getenv("BOT_USARNAME")
+ASSISTAN_USERNAME = getenv("ASSISTAN_USERNAME")
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
