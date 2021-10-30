9ffb6ee4e7603074c40ce9fd82708131416cb6c2
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2021-10-31 00:10:56 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -40,19 +40,6 @@ Ben {bot}! Sesli sohbetlerde müzik çalabilen botum. Ban yetkisiz, Ses yönetim
      disable_web_page_preview=True
     )
 
-@Client.on_message(filters.command(["yenile", f"yenile@{BOT_USERNAME}"]) & ~filters.private & ~filters.channel)
-async def yenile(_, message: Message):
-      await message.reply_text("""**Yeniden başlatıldı. Bot çalışıyor ✅**""",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "⚙ Geliştirici", url="https://t.me/Bir_Beyfendi")
-                ]
-            ]
-        )
-   )
-
 @Client.on_message(command(["bilgi", f"bilgi@{BOT_USERNAME}"])) 
 async def bilgi(_, message: Message):
       await message.reply_text(f"**Selam {message.from_user.mention}!\n Bu botun bilgi menüsü 🤩\n\n ▶️ /oynat - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /oynat <song name> - istediğiniz şarkıyı çal\n 🔴 /ytp <Sorgu> - youtube üzerinden çalma\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n 🔄 /yenile botu tekrar başlatır\n\n Yalnızca yöneticiler için..\n ▶️ /devam - şarkı çalmaya devam et\n ⏹ /bitir - müzik çalmayı durdurma\n 🔼 /ver botun sadece yönetici için kullanılabilir olan komutlarını kullanabilmesi için kullanıcıya yetki ver\n 🔽 /al botun yönetici komutlarını kullanabilen kullanıcının yetkisini al\n 🎚 /ses asistan hesabın ses seviyesini kontrol et\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.**", 
