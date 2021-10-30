e04b4e332780d3539f8f6d493e07c8695166c30c
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2021-10-30 23:24:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,10 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTAN_USERNAME = getenv("ASSISTAN_USERNAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
+PMPERMİT = getenv("PMPERMIT", none)
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
