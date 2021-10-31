570e6cd4dc4cceea8a48bd52fce8c379217af201
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2021-11-01 00:06:07 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,7 +9,7 @@ from helpers.filters import command, other_filters2
 async def start(client, message):
      msg_ = await message.reply_photo(
                                       photo="https://i.ibb.co/MpdcXYM/IMG-20211023-135851-221.jpg")
-                                      caption=f"""**Merhaba, {message.from_user.mention} 🎵\n Ben {bot}!\n Sesli sohbetlerde müzik çalabilen botum.\n Ban(kullanıcıları yasaklama) yetkisine gerek olmadan, Sesli sohbetleri yönetme yetkisi, Mesaj silme yetkisi ve Bağlantı ile davet etme verip, Asistanı gruba ekleyiniz.\n Komutlar için /bilgi komutunu kullanın.**""",
+                                      caption="**Merhaba, {message.from_user.mention} 🎵\n Ben {bot}!\n Sesli sohbetlerde müzik çalabilen botum.\n Ban(kullanıcıları yasaklama) yetkisine gerek olmadan, Sesli sohbetleri yönetme yetkisi, Mesaj silme yetkisi ve Bağlantı ile davet etme verip, Asistanı gruba ekleyiniz.\n Komutlar için /bilgi komutunu kullanın.**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
