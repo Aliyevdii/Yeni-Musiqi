db47844372ce177afe56ec2c60c11031fc4012d6
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2021-10-31 23:59:54 +0300

    Update restart.py

--- handlers/restart.py
+++ handlers/restart.py
@@ -1,13 +1,15 @@
+from handlers import check_heroku
 from helpers.filters import command
 from pyrogram import Client, filters
 from pyrogram import Client, filters
-from helpers.decorators import sudo_users_only
+from helpers.decorators import authorized_users_only
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
 
 
-@Client.on_message(command(["yenile", "reboot"]) & ~filters.edited)
+@Client.on_message(command(["restart"]) & ~filters.edited)
 @sudo_users_only
-async def gib_yenile(client, message, hap):
+@check_heroku
+async def gib_restart(client, message, hap):
     msg_ = await message.reply_photo(
                                      photo="https://te.legra.ph/file/813885c3687f7a6277315.jpg", 
                                      caption="**Yenileniyor**\n**Lütfen bekleyin**\n**Muhtemelen 5 dakika sürecektir...**",
