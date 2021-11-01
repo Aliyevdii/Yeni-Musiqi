f181f010204608bc5b605b66713262a74b18c757
SakirBey1 <sakirhack81@gmail.com>
2021-11-01 22:34:58 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -6,54 +6,62 @@ from helpers.filters import command, other_filters2
 #Bir_Beyfendi
 
 @Client.on_message(command(["start", f"start@{BOT_USERNAME}"]))
-async def start(client: Client, message: Message):
-    await message.reply_photo(photo="https://i.ibb.co/MpdcXYM/IMG-20211023-135851-221.jpg",
-    caption="**Merhaba {} 🎵\n Ben {}!\n Sesli sohbetlerde müzik çalabilen botum.\n Ban(kullanıcıları yasaklama) yetkisine gerek olmadan, Sesli sohbetleri yönetme yetkisi, Mesaj silme yetkisi ve Bağlantı ile davet etme verip, Asistanı gruba ekleyiniz.\n Komutlar için /bilgi komutunu kullanın.**".format(
-message.from_user.mention, bot
-),
-reply_markup=keyboard
-    reply_markup=InlineKeyboardMarkup(
+async def start(_, message: Message):
+    await message.reply_photo("https://i.ibb.co/MpdcXYM/IMG-20211023-135851-221.jpg")
+    await message.reply_text(
+        f"""**Merhaba, {message.from_user.mention} 🎵
+Ben {bot}! Sesli sohbetlerde müzik çalabilen botum. Ban yetkisiz, Ses yönetimi yetkisi verip, Asistanı gruba ekleyiniz. Komutlar için /help komutunu kullanın.**""",
+
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ Grubuna ekle ➕", url="https://t.me/HerTeldenMuzikBot?startgroup=true"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🏷️ Destek Grubu", url="https://t.me/SohbetOdagi"
-                    )
-                ],
-                [
+                        "🏷️ Beni gruba ekle", url="https://t.me/HerTeldenMuzikBot?startgroup=a"
+                    ),
                     InlineKeyboardButton(
                         "🔧 Geliştirici", url = "https://t.me/Bir_Beyfendi"
+                    )
+                  ],[
+                    InlineKeyboardButton(
+                        "🛠 Kurucu" , url = "https://t.me/Mahoaga"
                     ),
                     InlineKeyboardButton(
                         "🔊 Asistan" , url = "https://t.me/HerTeldenAsistan"
                     )
-                ],
-                [ 
+                ],[ 
                     InlineKeyboardButton(
                         "🌀 Komutlar" , url = "https://telegra.ph/Komutlar-10-22"
                     ),
                     InlineKeyboardButton(
                         "🎮 Oyun Botu", url="https://t.me/BasitOyunBot"
-                    )
-                ]
+                    )]
             ]
         ),
      disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command(["reload", f"reload@{BOT_USERNAME}"]) & ~filters.private & ~filters.channel)
+async def reload(_, message: Message):
+      await message.reply_text("""**Yeniden başlatıldı. Bot çalışıyor ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚙ Geliştirici", url="https://t.me/Bir_Beyfendi")
+                ]
+            ]
+        )
    )
 
-@Client.on_message(command(["bilgi", f"bilgi@{BOT_USERNAME}"]))
-async def bilgi(_, message: Message):
-      await message.reply_text(f"**Selam {message.from_user.mention}!\n Bu botun bilgi menüsü 🤩\n\n ▶️ /oynat - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /oynat <song name> - istediğiniz şarkıyı çal\n 🔴 /ytp <Sorgu> - youtube üzerinden çalma\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n\n Yalnızca yöneticiler için..\n ▶️ /devam - şarkı çalmaya devam et\n ⏹ /bitir - müzik çalmayı durdurma\n 🔼 /ver botun sadece yönetici için kullanılabilir olan komutlarını kullanabilmesi için kullanıcıya yetki ver\n 🔽 /al botun yönetici komutlarını kullanabilen kullanıcının yetkisini al\n 🎚 /ses asistan hesabın ses seviyesini kontrol et\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.\n\n ❗ Not:\n Botun aktif çalışması için şu üç yetkiye ihtiyaç vardır:\n - Mesaj silme yetkisi,\n - Bağlantı ile davet etme yetkisi,\n - Sesli sohbeti yönetme yetkisi.**", 
+@Client.on_message(filters.command(["help", f"help@{BOT_USERNAME}"]))
+async def help(_, message: Message):
+      await message.reply_text(f"""**Selam {message.from_user.mention}!\n Bu botun yardım menüsü 🤩\n\n ▶️ /oynat - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /oynat <song name> - istediğiniz şarkıyı çal\n 🔴 /ytp <Sorgu> - youtube üzerinden çalma\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n\n Yalnızca yöneticiler için..\n ▶️ /devam - şarkı çalmaya devam et\n ⏹ /bitir - müzik çalmayı durdurma\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.**""",
       reply_markup=InlineKeyboardMarkup(
-             [
-                 [
-                     InlineKeyboardButton(
-                         "⚙ Geliştirici", url="https://t.me/Bir_Beyfendi")
-                 ]
-             ]
-         )
-    )
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚙ Geliştirici", url="https://t.me/Bir_Beyfendi")
+                ]
+            ]
+        )
+   )
