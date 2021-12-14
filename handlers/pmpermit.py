aea1b0dfd07b3248ed09a5a621ecc2d534bd7702
Aliyevdii <95511686+Aliyevdii@users.noreply.github.com>
2021-12-14 18:02:52 +0400

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,7 +5,7 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Merhaba, Bu bir müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n   - Sohbete izin yok\n   - Bu sohbette spam yapmayın \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUBUNUZDAN YASAKLANMADIĞINA EMİN OLUN VE GRUBUNUZA MANUEL OLARAK EKLEYİN.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsanız Yöneticinin iletinizi göreceği anlamına gelir.\n    - Bu kullanıcıyı gizli gruplara eklemeyin.\n   - Özel bilgileri burada paylaşmayınız. 📚 Bilgi için @Bir_Beyfendi\n\n")
+  await USER.send_message(message.chat.id,"Merhaba, Bu bir müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n   - Sohbete izin yok\n   - Bu sohbette spam yapmayın \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUBUNUZDAN YASAKLANMADIĞINA EMİN OLUN VE GRUBUNUZA MANUEL OLARAK EKLEYİN.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsanız Yöneticinin iletinizi göreceği anlamına gelir.\n    - Bu kullanıcıyı gizli gruplara eklemeyin.\n   - Özel bilgileri burada paylaşmayınız. 📚 Bilgi için @NEXUS_MMC\n\n")
   return                        
 
  
